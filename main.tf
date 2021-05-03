c5fdbd3990dc580c42c90623c1df5c8989f5f07e
Thomas Vu <thomas.vu.it@gmail.com>
2021-05-03 11:13:46 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
+
 provider "aws" {
   region = var.region
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
-  name        = var.name
-  region = var.region
-  prefix = var.prefix
+  source  = "app.terraform.io/thrillho-first/s3-webapp/aws"
+  name    = var.name
+  region  = var.region
+  prefix  = var.prefix
   version = "1.0.0"
 }
